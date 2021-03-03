87cac5d9fc54b5448449f3236dab33ea135f4ac1
NickMortimer <nick.mortimer@csiro.au>
2021-03-03 16:56:20 +0800

    survey->Survey

--- src/processP4RTK.py
+++ src/processP4RTK.py
@@ -145,8 +145,8 @@ def task_plot_surveys():
         def process_survey(dependencies, targets,apikey):
             drone =pd.read_csv(list(dependencies)[0],index_col='TimeStamp',parse_dates=['TimeStamp'])
             px.set_mapbox_access_token(apikey)
-            fig = px.scatter_mapbox(drone, hover_name='survey', lat="Latitude", lon="Longitude",  
-                                    mapbox_style="satellite-streets",color="survey", size_max=30, zoom=10)
+            fig = px.scatter_mapbox(drone, hover_name='Survey', lat="Latitude", lon="Longitude",  
+                                    mapbox_style="satellite-streets",color="Survey", size_max=30, zoom=10)
             fig.update_layout(mapbox_style="satellite-streets")
             plotly.offline.plot(fig, filename=list(targets)[0],auto_open = False)
             
