9b7ec2281b68cdca3eb67d83b9e7f7af8aad208c
NickMortimer <nick.mortimer@csiro.au>
2022-02-01 14:44:54 +0800

    added bat

--- src/initalise.py
+++ src/initalise.py
@@ -26,17 +26,10 @@ import shapely.wkt
 from shapely.geometry import MultiPoint
 import xarray as xr
 import rasterio as rio
+from utils import convert_wgs_to_utm
 
 
-def convert_wgs_to_utm(lon, lat):
-    utm_band = str(int((np.floor((lon + 180) / 6 ) % 60) + 1))
-    if len(utm_band) == 1:
-        utm_band = '0'+utm_band
-    if lat >= 0:
-        epsg_code = '326' + utm_band
-    else:
-        epsg_code = '327' + utm_band
-    return epsg_code
+
 
 def task_make_area_list():
         def split(path):
