3d8cd1b99264425db8fdcd30d6bcb2b86ecb8f6e
NickMortimer <nick.mortimer@csiro.au>
2022-02-02 14:37:56 +0800

    add survey to build

--- src/build_DJI4Pro.bat
+++ src/build_DJI4Pro.bat
@@ -1,6 +1,7 @@
 set config=%1
 doit -f initalise.py  config=%config%
 doit -f exifdata.py  config=%config%
+doit -f surveys.py config=%config%
 doit -f DJIP4Pro.py  config=%config%
 doit -f reports.py  config=%config%
 
