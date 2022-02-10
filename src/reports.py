df9ff086db1faa484b79af57ea6f4533f97c568c
NickMortimer <nick.mortimer@csiro.au>
2022-02-10 13:49:29 +0800

    back to old

--- src/reports.py
+++ src/reports.py
@@ -86,7 +86,7 @@ def task_plot_surveys():
     def process_survey(dependencies, targets,apikey):
         drone =pd.read_csv(list(dependencies)[0],index_col='TimeStamp',parse_dates=['TimeStamp'])
         px.set_mapbox_access_token(apikey)
-        fig = px.scatter_mapbox(drone, hover_name='NewName', lat="Latitude", lon="Longitude",  
+        fig = px.scatter_mapbox(drone, hover_name='SurveyId', lat="Latitude", lon="Longitude",  
                                 mapbox_style="satellite-streets",color="SurveyId", size_max=30, zoom=10)
         fig.update_layout(mapbox_style="satellite-streets")
         plotly.offline.plot(fig, filename=list(targets)[0],auto_open = False)
