07d3ebecda5cdd5be7fb6795d3a9cefd677b9810
Nick Mortimer <nick.mortimer@csiro.au>
2023-02-10 09:49:42 +0800

    updated lable me

--- src/labelme.py
+++ src/labelme.py
@@ -2,6 +2,7 @@ from genericpath import exists
 import os
 import glob
 import shutil
+from tkinter import E
 import doit
 import glob
 import os
@@ -53,13 +54,14 @@ from PIL import Image
 def loadshapes(file):
     lines =[]
     with open(file, "r") as read_file:
-        while read_file:
-            line =read_file.readline()
-            if "imagePath" in line:
-                break
-            lines.append(line)
-    lines[-1] ='  ]}\n'
-    data = json.loads(''.join(lines).replace("\n", "").replace("'", '"').replace('u"', '"'))
+        data = json.load(read_file)
+    #     while read_file:
+    #         line =read_file.readline()
+    #         if "imagePath" in line:
+    #             break
+    #         lines.append(line)
+    # lines[-1] ='  ]}\n'
+    #data = json.loads(''.join(lines).replace("\n", "").replace("'", '"').replace('u"', '"'))
     data =pd.DataFrame(data['shapes'])
     data['FilePath'] =file             
     return(data)
@@ -117,8 +119,8 @@ def task_process_matchup():
         source_file.index = source_file.NewName.apply(lambda x:os.path.splitext(x)[0])
         lableme.index = lableme.FilePath.apply(lambda x: os.path.splitext(os.path.basename(x))[0])
         output =lableme.join(source_file)
-        output.index.name='Key'
-        output.to_csv(targets[0])
+        output.index.name='Key'     
+        output[~output.UtmCode.isna()].to_csv(targets[0])
     config = {"config": get_var('config', 'NO')}
     with open(config['config'], 'r') as ymlfile:
         cfg = yaml.load(ymlfile, yaml.SafeLoader)
@@ -136,148 +138,148 @@ def task_process_matchup():
     }   
     
     
-def task_make_yolo_training_images():
-    def process_yoloset(dependencies, targets):
-        os.makedirs(os.path.dirname(targets[0]),exist_ok=True)
-        imagedir = os.path.join(os.path.dirname(targets[0]),'images')
-        os.makedirs(imagedir,exist_ok=True)
-        sourcefile = pd.read_csv(dependencies[0])
-        good =sourcefile[~sourcefile.label.isin(['done','don,e','gcp'])]
-        good.points = good.points.apply(ast.literal_eval)
-        classes = list(good.label.unique())
-        classes.sort()
-        images =good.groupby('FilePath')
-        for file,data in images:
-            Imgdest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.JPG')
-            if not os.path.exists(Imgdest):
-                shutil.copy(os.path.splitext(file)[0]+'.JPG',
-                            os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.JPG'))
-            jsondest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.json')
-            if not os.path.exists(jsondest):
-                shapes =loadshapes(file)
-                shapes.drop(['FilePath','flags','group_id'],axis=1)[~shapes.label.isin(['done','don,e','gcp'])].to_json(jsondest)
-            txtdest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.TXT')
-            if not os.path.exists(txtdest):
-                with open(txtdest,'w') as datafile:
-                    for index,row in data.iterrows():
-                        cla = classes.index(row.label)
-                        points = row.points
-                        if len(points)==2: #one animal
-                            datafile.write(f'{classes.index(row.label)} {points[0][0]/row.ImageWidth} {points[0][1]/row.ImageHeight} '\
-                                            f'{2*abs(points[0][0]-points[1][0])/row.ImageWidth}' \
-                                            f'{2*abs(points[0][1]-points[1][1])/row.ImageHeight}\n')
+# def task_make_yolo_training_images():
+#     def process_yoloset(dependencies, targets):
+#         os.makedirs(os.path.dirname(targets[0]),exist_ok=True)
+#         imagedir = os.path.join(os.path.dirname(targets[0]),'images')
+#         os.makedirs(imagedir,exist_ok=True)
+#         sourcefile = pd.read_csv(dependencies[0])
+#         good =sourcefile[~sourcefile.label.isin(['done','don,e','gcp'])]
+#         good.points = good.points.apply(ast.literal_eval)
+#         classes = list(good.label.unique())
+#         classes.sort()
+#         images =good.groupby('FilePath')
+#         for file,data in images:
+#             Imgdest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.JPG')
+#             if not os.path.exists(Imgdest):
+#                 shutil.copy(os.path.splitext(file)[0]+'.JPG',
+#                             os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.JPG'))
+#             jsondest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.json')
+#             if not os.path.exists(jsondest):
+#                 shapes =loadshapes(file)
+#                 shapes.drop(['FilePath','flags','group_id'],axis=1)[~shapes.label.isin(['done','don,e','gcp'])].to_json(jsondest)
+#             txtdest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.TXT')
+#             if not os.path.exists(txtdest):
+#                 with open(txtdest,'w') as datafile:
+#                     for index,row in data.iterrows():
+#                         cla = classes.index(row.label)
+#                         points = row.points
+#                         if len(points)==2: #one animal
+#                             datafile.write(f'{classes.index(row.label)} {points[0][0]/row.ImageWidth} {points[0][1]/row.ImageHeight} '\
+#                                             f'{2*abs(points[0][0]-points[1][0])/row.ImageWidth}' \
+#                                             f'{2*abs(points[0][1]-points[1][1])/row.ImageHeight}\n')
                     
                  
                  
-        with open(targets[0],'w') as obnames:
-            obnames.writelines(map(lambda x:x+'\n', classes))
+#         with open(targets[0],'w') as obnames:
+#             obnames.writelines(map(lambda x:x+'\n', classes))
         
-    config = {"config": get_var('config', 'NO')}
-    with open(config['config'], 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, yaml.SafeLoader)
-    basepath = os.path.dirname(config['config'])
-    file_dep = os.path.join(basepath,cfg['paths']['process'],'labelmematchup.csv')
-    target = os.path.join(basepath,cfg['paths']['output'],'yolo','data','obj.names')        
-    return {
-        'actions':[process_yoloset],
-        'file_dep':[file_dep],
-        'targets':[target],
-        'clean':True,
-    }   
+#     config = {"config": get_var('config', 'NO')}
+#     with open(config['config'], 'r') as ymlfile:
+#         cfg = yaml.load(ymlfile, yaml.SafeLoader)
+#     basepath = os.path.dirname(config['config'])
+#     file_dep = os.path.join(basepath,cfg['paths']['process'],'labelmematchup.csv')
+#     target = os.path.join(basepath,cfg['paths']['output'],'yolo','data','obj.names')        
+#     return {
+#         'actions':[process_yoloset],
+#         'file_dep':[file_dep],
+#         'targets':[target],
+#         'clean':True,
+#     }   
     
     
-def task_crop_training_images():
-    def process_crop(dependencies, targets,size=256):
-        basepath = os.path.dirname(targets[0])
-        os.makedirs(basepath,exist_ok=True)
-        sourcefile = pd.read_csv(dependencies[0])
-        good =sourcefile[~sourcefile.label.isin(['done','don,e','gcp'])]
-        good.points = good.points.apply(ast.literal_eval)
-        classes = list(good.label.unique())
-        list(map(lambda x:os.makedirs(os.path.join(basepath,x),exist_ok=True), classes))
-        classes.sort()
-        images =good.groupby('FilePath')
-        for file,data in images:
-                imageObject = Image.open(os.path.splitext(file)[0]+'.JPG')
-                imagebase = os.path.splitext(os.path.basename(file))[0]
-                counter=0
-                for index,row in data.iterrows():
-                    cla = classes.index(row.label)
-                    points = row.points
-                    if len(points)==2: #one animal
-                        counter = counter+1
-                        output =os.path.join(basepath,row.label,f'{imagebase}_{counter:03d}.JPG')
-                        cropped = imageObject.crop((int(points[0][0] - size/2),
-                                                   int(points[0][1] - size/2),
-                                                   int(points[0][0] + size/2),
-                                                   int(points[0][1] + size/2)))
+# def task_crop_training_images():
+#     def process_crop(dependencies, targets,size=256):
+#         basepath = os.path.dirname(targets[0])
+#         os.makedirs(basepath,exist_ok=True)
+#         sourcefile = pd.read_csv(dependencies[0])
+#         good =sourcefile[~sourcefile.label.isin(['done','don,e','gcp'])]
+#         good.points = good.points.apply(ast.literal_eval)
+#         classes = list(good.label.unique())
+#         list(map(lambda x:os.makedirs(os.path.join(basepath,x),exist_ok=True), classes))
+#         classes.sort()
+#         images =good.groupby('FilePath')
+#         for file,data in images:
+#                 imageObject = Image.open(os.path.splitext(file)[0]+'.JPG')
+#                 imagebase = os.path.splitext(os.path.basename(file))[0]
+#                 counter=0
+#                 for index,row in data.iterrows():
+#                     cla = classes.index(row.label)
+#                     points = row.points
+#                     if len(points)==2: #one animal
+#                         counter = counter+1
+#                         output =os.path.join(basepath,row.label,f'{imagebase}_{counter:03d}.JPG')
+#                         cropped = imageObject.crop((int(points[0][0] - size/2),
+#                                                    int(points[0][1] - size/2),
+#                                                    int(points[0][0] + size/2),
+#                                                    int(points[0][1] + size/2)))
 
-                        cropped.save(output)                    
-        with open(targets[0],'w') as obnames:
-            obnames.writelines(map(lambda x:x+'\n', classes))
+#                         cropped.save(output)                    
+#         with open(targets[0],'w') as obnames:
+#             obnames.writelines(map(lambda x:x+'\n', classes))
         
-    config = {"config": get_var('config', 'NO')}
-    with open(config['config'], 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, yaml.SafeLoader)
-    basepath = os.path.dirname(config['config'])
-    file_dep = os.path.join(basepath,cfg['paths']['process'],'labelmematchup.csv')
-    target = os.path.join(basepath,cfg['paths']['output'],'train','obj.names')        
-    return {
-        'actions':[process_crop],
-        'file_dep':[file_dep],
-        'targets':[target],
-        'clean':True,
-    }     
+#     config = {"config": get_var('config', 'NO')}
+#     with open(config['config'], 'r') as ymlfile:
+#         cfg = yaml.load(ymlfile, yaml.SafeLoader)
+#     basepath = os.path.dirname(config['config'])
+#     file_dep = os.path.join(basepath,cfg['paths']['process'],'labelmematchup.csv')
+#     target = os.path.join(basepath,cfg['paths']['output'],'train','obj.names')        
+#     return {
+#         'actions':[process_crop],
+#         'file_dep':[file_dep],
+#         'targets':[target],
+#         'clean':True,
+#     }     
 
 
-def task_make_training_images():
-    def process_yoloset(dependencies, targets):
-        os.makedirs(os.path.dirname(targets[0]),exist_ok=True)
-        imagedir = os.path.join(os.path.dirname(targets[0]),'images')
-        os.makedirs(imagedir,exist_ok=True)
-        sourcefile = pd.read_csv(dependencies[0])
-        good =sourcefile[~sourcefile.label.isin(['done','don,e','gcp'])]
-        good.points = good.points.apply(ast.literal_eval)
-        classes = list(good.label.unique())
-        classes.sort()
-        images =good.groupby('FilePath')
-        for file,data in images:
-            Imgdest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.JPG')
-            if not os.path.exists(Imgdest):
-                shutil.copy(os.path.splitext(file)[0]+'.JPG',
-                            os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.JPG'))
-            jsondest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.json')
-            if not os.path.exists(jsondest):
-                shapes =loadshapes(file)
-                shapes.drop(['FilePath','flags','group_id'],axis=1)[~shapes.label.isin(['done','don,e','gcp'])].to_json(jsondest)
-            txtdest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.TXT')
-            if not os.path.exists(txtdest):
-                with open(txtdest,'w') as datafile:
-                    for index,row in data.iterrows():
-                        cla = classes.index(row.label)
-                        points = row.points
-                        if len(points)==2: #one animal
-                            datafile.write(f'{classes.index(row.label)} {points[0][0]/row.ImageWidth} {points[0][1]/row.ImageHeight} '\
-                                            f'{2*abs(points[0][0]-points[1][0])/row.ImageWidth}' \
-                                            f'{2*abs(points[0][1]-points[1][1])/row.ImageHeight}\n')
+# def task_make_training_images():
+#     def process_yoloset(dependencies, targets):
+#         os.makedirs(os.path.dirname(targets[0]),exist_ok=True)
+#         imagedir = os.path.join(os.path.dirname(targets[0]),'images')
+#         os.makedirs(imagedir,exist_ok=True)
+#         sourcefile = pd.read_csv(dependencies[0])
+#         good =sourcefile[~sourcefile.label.isin(['done','don,e','gcp'])]
+#         good.points = good.points.apply(ast.literal_eval)
+#         classes = list(good.label.unique())
+#         classes.sort()
+#         images =good.groupby('FilePath')
+#         for file,data in images:
+#             Imgdest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.JPG')
+#             if not os.path.exists(Imgdest):
+#                 shutil.copy(os.path.splitext(file)[0]+'.JPG',
+#                             os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.JPG'))
+#             jsondest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.json')
+#             if not os.path.exists(jsondest):
+#                 shapes =loadshapes(file)
+#                 shapes.drop(['FilePath','flags','group_id'],axis=1)[~shapes.label.isin(['done','don,e','gcp'])].to_json(jsondest)
+#             txtdest = os.path.join(imagedir,os.path.splitext(os.path.basename(file))[0]+'.TXT')
+#             if not os.path.exists(txtdest):
+#                 with open(txtdest,'w') as datafile:
+#                     for index,row in data.iterrows():
+#                         cla = classes.index(row.label)
+#                         points = row.points
+#                         if len(points)==2: #one animal
+#                             datafile.write(f'{classes.index(row.label)} {points[0][0]/row.ImageWidth} {points[0][1]/row.ImageHeight} '\
+#                                             f'{2*abs(points[0][0]-points[1][0])/row.ImageWidth}' \
+#                                             f'{2*abs(points[0][1]-points[1][1])/row.ImageHeight}\n')
                     
                  
                  
-        with open(targets[0],'w') as obnames:
-            obnames.writelines(map(lambda x:x+'\n', classes))
+#         with open(targets[0],'w') as obnames:
+#             obnames.writelines(map(lambda x:x+'\n', classes))
         
-    config = {"config": get_var('config', 'NO')}
-    with open(config['config'], 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, yaml.SafeLoader)
-    basepath = os.path.dirname(config['config'])
-    file_dep = os.path.join(basepath,cfg['paths']['process'],'labelmematchup.csv')
-    target = os.path.join(basepath,cfg['paths']['output'],'yolo','obj.names')        
-    return {
-        'actions':[process_yoloset],
-        'file_dep':[file_dep],
-        'targets':[target],
-        'clean':True,
-    }      
+#     config = {"config": get_var('config', 'NO')}
+#     with open(config['config'], 'r') as ymlfile:
+#         cfg = yaml.load(ymlfile, yaml.SafeLoader)
+#     basepath = os.path.dirname(config['config'])
+#     file_dep = os.path.join(basepath,cfg['paths']['process'],'labelmematchup.csv')
+#     target = os.path.join(basepath,cfg['paths']['output'],'yolo','obj.names')        
+#     return {
+#         'actions':[process_yoloset],
+#         'file_dep':[file_dep],
+#         'targets':[target],
+#         'clean':True,
+#     }      
     
 # def task_process_movefiles():
 #     def process_move(dependencies, targets,outputpath):
@@ -309,8 +311,13 @@ def task_calculate_positions():
             def calcRealworld(item):
                 #-14.772 hieght at Exmouth
                 localdrone = P4rtk(data,crs)
-                localdrone.setdronepos(item.Eastingrtk,item.Northingrtk,item.EllipsoideHight+14.772,
-                                  (90+item.GimbalPitchDegree)*-1,item.GimbalRollDegree,-item.GimbalYawDegree+8) #item.GimbalPitchDegree-90,item.GimbalRollDegree,-item.GimbalYawDegree
+                if 'EllipsoideHight' in item:
+                    localdrone.setdronepos(item.Eastingrtk,item.Northingrtk,item.EllipsoideHight+14.772,
+                                    (90+item.GimbalPitchDegree)*-1,item.GimbalRollDegree,-item.GimbalYawDegree+8) #item.GimbalPitchDegree-90,item.GimbalRollDegree,-item.GimbalYawDegree
+                else:
+                    localdrone.setdronepos(item.Easting,item.Northing,pd.to_numeric(item.GPSAltitude.split(' ')[0]),
+                                    (90+item.GimbalPitchDegree)*-1,item.GimbalRollDegree,-item.GimbalYawDegree+8) #item.GimbalPitchDegree-90,item.GimbalRollDegree,-item.GimbalYawDegree
+                    
                 pos=localdrone.cameratorealworld(item.DewarpX,item.DewarpY)
                 item.EastingPntD = pos[0]
                 item.NorthingPntD = pos[1]
@@ -333,7 +340,8 @@ def task_calculate_positions():
             corrected =p4rtk.jpegtoreal(jpegpoints)
             drone[['DewarpX','DewarpY']] =corrected
             drone[['EastingPntJ','NorthingPntJ','EastingPntD','NorthingPntD']] = 0.
-            drone['EllipsoideHight']= pd.to_numeric(drone['EllipsoideHight'].str.split(',',expand=True)[0])
+            if 'EllipsoideHight' in drone.columns:
+                drone['EllipsoideHight']= pd.to_numeric(drone['EllipsoideHight'].str.split(',',expand=True)[0])
             drone = drone.apply(calcRealworld,axis=1)
             drone.to_csv(targets[0],index=False) 
 
