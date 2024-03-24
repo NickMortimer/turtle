8943afe4ee9afa4a2af9b160c1634fcab3adac1c
Nick Mortimer <nick.mortimer@csiro.au>
2024-03-24 18:46:45 +0800

    try again with docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,7 @@ release = 'alpha'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx_rtd_theme',
-]
+extensions = ['sphinx_rtd_theme']
 
 
 templates_path = ['_templates']
@@ -27,6 +25,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-
-html_theme = "sphinx_rtd_theme"
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
