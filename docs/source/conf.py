32ffc7c209ddf22c2c327855efdb580201a30758
Nick Mortimer <nick.mortimer@csiro.au>
2024-03-26 23:43:26 +0800

    Agin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ exclude_patterns = ['**.ipynb_checkpoints']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#html_theme = 'sphinx_rtd_theme'
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #html_static_path = ['_static']
