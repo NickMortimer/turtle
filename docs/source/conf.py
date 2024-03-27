c5989ce2e8503e385f67a87f58bf520ac6559bd8
Nick Mortimer <nick.mortimer@csiro.au>
2024-03-27 12:03:37 +0800

    agin

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,26 +11,27 @@ copyright = '2024, Nick Mortimer'
 author = 'Nick Mortimer'
 release = 'alpha'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# -- General configuration
 
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode'
-    ]
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = ['**.ipynb_checkpoints']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-import sphinx_rtd_theme
+# -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
